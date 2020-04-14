c8319b8281588f70af996ba9610c3f01d84d4980
Lakshmi Vasavi <tlakshmivasavi005@gmail.com>
2020-04-14 15:48:00 +0530

    cookie

---
+++ CarPoolingMVC/wwwroot/js/Cookies.js
@@ -0,0 +1,10 @@
+﻿function read_cookie(name) {
+    var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+}
+
+function save_cookie(name, value) {
+    var cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+    document.cookie = cookie;
+}
